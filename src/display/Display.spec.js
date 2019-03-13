7e888e33d9846708fea9ce4201bea7cef8443d9d
cwright0428 <cwright0428@yahoo.com>
2019-03-13 17:47:46 -0500

    adds test frameworks

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from 'react';
+import Display from './Display';
+import renderer from 'react-test-renderer';
+
+
+describe('<Display />', () => {
+    it('control the locked/unlocked gates and red and green leds', () => {
+
+    });
+
+    it('should match snapshot', () => {
+        const tree = renderer.create()
+
+        expect(tree.toJSON()).toMatchSnapshot()
+
+    });
+
+});
